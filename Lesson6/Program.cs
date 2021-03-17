1c0ecaddfb374798343f0ae30a9c93aacb2b3d3c
Alexey Goncharov <alex.goncharoff@gmail.com>
2021-03-17 14:18:26 +0300

    I5.5 - done

---
+++ Lesson6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cards = new Dictionary<int, string>();
+            cards.Add(1,"Spades");
+            cards.Add(2,"Clubs");
+            cards.Add(3,"Diamonds");
+            cards.Add(4,"Hearts");
+
+            Console.WriteLine("Please enter your number (1-4)");
+            var input = Console.Read();
+            bool check = (CharUnicodeInfo.GetDecimalDigitValue((char)input) > 0 && CharUnicodeInfo.GetDecimalDigitValue((char)input) <= 4);
+            if (check)
+            {
+                Console.WriteLine(cards[CharUnicodeInfo.GetDecimalDigitValue((char)input)]);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input");
+            }
+
+        }
+    }
+}
