40b9fec45ccff999891c042a3de6d2623133adc2
Alexey Goncharov <alex.goncharoff@gmail.com>
2021-03-18 20:23:14 +0300

    123

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -8,18 +8,19 @@ namespace Lesson6
     {
         static void Main(string[] args)
         {
-            // FifteenDotFive fifteenDotFive = new FifteenDotFive();
-            // FifteenDotSix fifteenDotSix = new FifteenDotSix();
-            // LOneDotFive lOneDotFive = new LOneDotFive();
-            //
-            // Console.WriteLine("Please enter your number (1-4)");
-            // var input = Convert.ToInt32(Console.ReadLine());
-            // fifteenDotFive.FifteenDotFiveSolution(input);
-            //
-            // Console.WriteLine("Please enter card number (6-14)");
-            // int input2 = Convert.ToInt32(Console.ReadLine());
-            // fifteenDotSix.FifteenDotSixSolution(input2);
-            // lOneDotFive.LOneDotFiveSolution();
+            FifteenDotFive fifteenDotFive = new FifteenDotFive();
+            FifteenDotSix fifteenDotSix = new FifteenDotSix();
+            LOneDotFive lOneDotFive = new LOneDotFive();
+            
+            Console.WriteLine("Please enter your number (1-4)");
+            var input = Convert.ToInt32(Console.ReadLine());
+            fifteenDotFive.FifteenDotFiveSolution(input);
+            
+            Console.WriteLine("Please enter card number (6-14)");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            fifteenDotSix.FifteenDotSixSolution(input2);
+            
+            lOneDotFive.LOneDotFiveSolution();
 
 
             Console.WriteLine("Enter number of points");
