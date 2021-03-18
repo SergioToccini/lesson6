32f6c5811c5fb8abb322696f31ddf286b9291d6b
Alexey Goncharov <alex.goncharoff@gmail.com>
2021-03-18 17:25:47 +0300

    +Circle task - error somewhere

--- Lesson6/Circle.cs
+++ Lesson6/Circle.cs
@@ -7,8 +7,7 @@ namespace Lesson6
         private static readonly Random Random = new Random();
 
         private static int _countInCircle = 0;
-        private static int _countAll = 0;
-        
+
         private static double RandomTo1()
         {
             return Random.NextDouble();
@@ -16,18 +15,18 @@ namespace Lesson6
 
         public static void Counter(int countAll)
         {
-            _countAll = countAll;
-            for (int i = 0; i < _countAll; i++)
+            for (int i = 0; i < countAll; i++)
             {
                 double x = RandomTo1();
                 double y = RandomTo1();
-                if (Math.Sqrt(Math.Pow(x,2))+Math.Pow(y,2) <=1)
+
+                if (Math.Sqrt(Math.Pow(x,2)) + Math.Pow(y,2) <=1)
                 {
                     _countInCircle++;
                 }
             }
-
-            double pi = 4*(double) _countInCircle/_countAll;
+            
+            double pi = 4 * (double)_countInCircle/countAll;
             Console.WriteLine($"Pi = {pi}");
 
         }
